3893e46eb007a112020ef4f6cefbb0267b93e4c9
hemiblaze <68324167+hemiblaze@users.noreply.github.com>
2021-03-19 17:03:40 +0530

    Create ChickenBurger.java

---
+++ Day2/session1/Builder Pattern/ChickenBurger.java
@@ -0,0 +1,16 @@
+package BuilderPattern;
+public class ChickenBurger extends Burger {
+
+	@Override
+	public String name() {
+		return "Chicken Burger";
+	}
+
+	@Override
+	public float price() {
+		return 100.0f;
+	}
+	
+	
+
+}
