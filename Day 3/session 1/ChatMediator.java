716c0df1d318c7e9638041e88dff96539cdcecb7
hemiblaze <68324167+hemiblaze@users.noreply.github.com>
2021-03-21 16:11:10 +0530

    Update ChatMediator.java

--- Day 3/session 1/ChatMediator.java
+++ Day 3/session 1/ChatMediator.java
@@ -18,7 +18,6 @@ public class ChatMediator implements IChatMediator {
 	public void SendMessage(String message, IUser sender) {
 		for(IUser user: users) {
 			
-			//sender will not receive message
 			if(user != sender)
 				user.ReceiveMessage(message);
 		}
